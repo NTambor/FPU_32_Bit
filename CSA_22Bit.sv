44e188494a98514fe5eee5cdf72459d28a8c654a
NTambor <Niko.tambo21@gmail.com>
2026-03-01 23:57:06 -0800

    ADDer work

--- CSA_22Bit.sv
+++ CSA_22Bit.sv
@@ -1,30 +1,62 @@
 `timescale 1ns/1ns
 module CSA_22Bit(
     input logic clk,
-    input logic [21:0] a,
-    input logic [21:0] b,
-    output logic [22:0] s,
+    input logic op,
+    input logic [22:0] a,
+    input logic asign,
+    input logic [22:0] b,
+    input logic bsign,
+    output logic [23:0] s,
 );
     //Logic Layer 1
-    wire [21:0] s1, c1, c2
+    wire [21:0] s1, c1, c2;
+    
+
+    fulladderclk ha_instance(
+        .clk(clk),
+        .a(a[0]),
+        .b((sub) ? b[0] : ~b[0]),
+        .cin(sub),
+        .sum(s1[0]),
+        .cout(c1[0]));
+
     genvar i;
     generate
-        for (i = 0; i < 22; i = i + 1) begin : threads
+        for (i = 1; i < 22; i = i + 1) begin
             fulladderclk ha_instance(
                 .clk(clk),
                 .a(a[i]),
-                .b(b[i]),
+                .b((sub)? b[i] : ~b[i]),
                 .cin(1'b0),
                 .sum(s1[i]),
                 .cout(c1[i]));
             fulladderclk fa_instance(
                 .clk(clk),
                 .a(s1[i]),
-                .b(c1[i+1]),
-                .cin(c2[i]),
+                .b(c1[i-1]),
+                .cin(c2[i-1]),
+                .sum(s[i]),
+                .cout(c2[i]));
+        end
+        for (i = 1; i < 22; i = i + 1) begin
+            fulladderclk fa_instance(
+                .clk(clk),
+                .a(s1[i]),
+                .b(c1[i-1]),
+                .cin(c2[i-1]),
                 .sum(s[i]),
                 .cout(c2[i]));
         end
     endgenerate
+    fulladderclk fa_instance(
+        .clk(clk),
+        .a(1'b0),
+        .b(c1[21]),
+        .cin(c2[21]),
+        .sum(s[22]),
+        .cout(cout));
+    always @(posedge clk) begin
+        if 
+    end
 
 endmodule
--- FP32_ADD.sv
+++ FP32_ADD.sv
@@ -6,21 +6,68 @@ module FP32_ADD #(
     output logic [8:0] exp,
     output logic [23:0] mantisa
 );
+// first clk cycle compare the exponents and add 1 to mantisas
+reg [7:0] expmiddle;
+reg [7:0] expmiddlea;
+reg [7:0] expmiddleb;
+reg [23:0] mantisaA0;
+reg [23:0] mantisaB0;
+
+// second clk cycle shift the mantisa
+reg [63:0] mantisaAshift;
+reg [63:0] mantisaBshift;
+
+// Third clk add mantissa
+reg [63:0] mantisaresult;
+
+// Four Normalize 
+
+// Five Adjust exponent and round
+
 
 //step one compare expa and expb and allign
 assign expmiddle = expa | expb;
-assign expmiddlea = expmidle ^ expa;
-assign expmiddleb = expmidle ^ expb;
-
+assign expmiddlea = (expa | expb) ^ expa;
+assign expmiddleb = (expa | expb) ^ expb;
+mantisaA += 1;
+mantisaB += 1;
 
 
 //step two shift mantisas so they allign together
-mantisaAshift = mantisaA >> expmiddlea;
-mantisaBshift = mantisaB >> expmiddleb
+mantisaAshift = mantisaA0 >> expmiddlea;
+mantisaBshift = mantisaB0 >> expmiddleb;
 
-//step three adder 
+//step three adder
+wire sub;
+assign sub = bsign ^ op ^ asign; 
 CSA csa_matisa();
 
+always @(posedge clk) begin
+    // first clk cycle compare the exponents and add 1 to mantisas
+    expmiddle = expa | expb;
+    expmiddlea = (expa | expb) ^ expa;
+    expmiddleb = (expa | expb) ^ expb;
+    mantisaA0 = {1'b0,mantisaA};
+    mantisaB0 = {1'b0,mantisaB};
+
+    // second clk cycle shift the mantisa
+    mantisaAshift = mantisaA0 >> expmiddlea;
+    mantisaBshift = mantisaB0 >> expmiddleb;
+
+    // Third clk add mantissa
+    mantisaresult = mantisaAshift+ mantisaBshift
+
+    // Four Normalize 
+    for(i = 0; i<64; i++)begin
+        if (mantisaresult[i] == 1'b1 & )begin
+            
+        end
+    end
+
+    // Five Adjust exponent and round
+
+
+end
 
 
 endmodule
