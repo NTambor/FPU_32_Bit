be6ceb77e0c5b1e8d21f45f6e0b3add3be540459
Agustin <39395295+AgustinHR@users.noreply.github.com>
2026-03-10 16:15:55 -0700

    Create partial_product_generator.sv

---
+++ partial_product_generator.sv
@@ -0,0 +1,23 @@
+
+// N = Multiplicand 
+// M = Multiplier 
+// W = Width
+
+module partial_product_generator #(parameter N = 24,
+                                  parameter M = 24,
+                                  parameter W = N + M)(
+    input logic [N-1:0] A,
+    input logic [M-1:0] B,
+    output logic [W-1:0] partial_products_shifted [M-1:0]
+);
+
+    genvar i;
+    generate
+        for (i = 0; i < M; i = i + 1) begin
+            logic [N-1:0] masked;
+            assign masked = A & {N{B[i]}};
+            assign partial_products_shifted[i] = { {(W-N-i){1'b0}}, masked, {i{1'b0}}  };
+        end
+    endgenerate
+
+endmodule
