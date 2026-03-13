88c66c0aff60f332008ba38aabf4b7633463abb2
Agustin <39395295+AgustinHR@users.noreply.github.com>
2026-03-12 18:40:26 -0700

    Using Packed Arrays Instead Of Unpacked

--- partial_product_generator.sv
+++ partial_product_generator.sv
@@ -1,14 +1,10 @@
 
-// N = Multiplicand 
-// M = Multiplier 
-// W = Width
-
 module partial_product_generator #(parameter N = 24,
                                   parameter M = 24,
                                   parameter W = N + M)(
     input logic [N-1:0] A,
     input logic [M-1:0] B,
-    output logic [W-1:0] partial_products_shifted [M-1:0]
+    output logic [M-1:0] [W-1:0] partial_products_shifted
 );
 
     genvar i;
@@ -19,5 +15,5 @@ module partial_product_generator #(parameter N = 24,
             assign partial_products_shifted[i] = { {(W-N-i){1'b0}}, masked, {i{1'b0}}  };
         end
     endgenerate
-
+    
 endmodule
